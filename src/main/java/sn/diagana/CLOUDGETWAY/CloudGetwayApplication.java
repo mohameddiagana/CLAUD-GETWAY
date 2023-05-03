52a51e85abe6fcbb90d200721625b2a8dbb0c81d
mohameddiagana <mohameddiagana807@gmail.com>
2023-05-03 20:24:50 +0000

    init commit

---
+++ src/main/java/sn/diagana/CLOUDGETWAY/CloudGetwayApplication.java
@@ -0,0 +1,17 @@
+package sn.diagana.CLOUDGETWAY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+//@EnableDiscoveryClient
+public class CloudGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGetwayApplication.class, args);
+	}
+
+}
